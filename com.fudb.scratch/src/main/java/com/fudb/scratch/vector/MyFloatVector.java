00af412e05322f8762c98aa93ffaa853f11e555d
Elon Azoulay <elon.azoulay@gmail.com>
2021-05-15 22:57:21 -0700

    Update to use loopbound

--- com.fudb.scratch/src/main/java/com/fudb/scratch/vector/MyFloatVector.java
+++ com.fudb.scratch/src/main/java/com/fudb/scratch/vector/MyFloatVector.java
@@ -2,20 +2,40 @@ package com.fudb.scratch.vector;
 
 import jdk.incubator.vector.FloatVector;
 
+import java.util.Arrays;
+
+import static java.util.Objects.requireNonNull;
+
 public class MyFloatVector {
-    private final FloatVector first;
-    private final FloatVector second;
+    private final float[] first;
+    private final float[] second;
     private final int size;
 
     public MyFloatVector(float[] a, float[] b) {
-        this.first = FloatVector.fromArray(FloatVector.SPECIES_128, a, 0);
-        this.second = FloatVector.fromArray(FloatVector.SPECIES_128, b, 0);
+        requireNonNull(a);
+        requireNonNull(b);
+        if (a.length != b.length) {
+            throw new IllegalStateException("a and b must have equal lengths");
+        }
+        this.first = Arrays.copyOf(a, a.length);
+        this.second = Arrays.copyOf(b, b.length);
         this.size = a.length;
     }
 
     public float[] radius() {
         float[] result = new float[size];
-        first.pow(2).add(second.pow(2)).sqrt().intoArray(result, 0);
+        int upperBound = FloatVector.SPECIES_PREFERRED.loopBound(size);
+        int i = 0;
+        for (; i < upperBound; i += FloatVector.SPECIES_PREFERRED.length()) {
+            var va = FloatVector.fromArray(FloatVector.SPECIES_PREFERRED, first, i);
+            var vb = FloatVector.fromArray(FloatVector.SPECIES_PREFERRED, second, i);
+            va.pow(2);
+            vb.pow(2);
+            va.add(vb).sqrt().intoArray(result, i);
+        }
+        for (; i < size; i++) {
+            result[i] = ((Double) Math.sqrt(first[i] * first[i] + second[i] * second[i])).floatValue();
+        }
         return result;
     }
 }
